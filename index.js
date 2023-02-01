a92896c0202bd45047b63509d0df8d86ff00c2b3
callixmiller <calmill413@gmail.com>
2023-02-01 18:16:59 -0500

    index.js passing all tests, weird error message did pop up with this test

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        })
+    }
+    return fetch('http://localhost:3000/users', configObj)
+        .then(res => res.json())
+        .then(body => addNewContact(body.id))
+        .catch(error => document.querySelector('body').innerHTML = error)
+}
+
+function addNewContact(newId){
+    document.getElementsByTagName('body')[0].innerHTML = newId
+    document.body.append(message)
+}
